d3b4ad39cb020d1381acdf2c66ec544acd5facf6
Artiom_Regino <artiom_regino@epam.com>
2021-03-11 23:57:30 +0300

    feat: add typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0) {
+    return 0;
+  }
+  
+  return array.reduce((previous, current) => previous < current ? previous : current );
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0) {
+    return 0;
+  }
+  
+  return array.reduce((previous, current) => previous > current ? previous : current );
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0) {
+    return 0;
+  }
+  
+  return array.reduce((previous, current) => previous + current ) / array.length;
 }
